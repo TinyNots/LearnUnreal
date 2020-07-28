22e110e3f43edc1f2f0a6afbdf59a6113236a907
Tan Soon Seang <alextan1751@gmail.com>
2020-07-28 16:07:22 +0900

    Error fixed

--- Source/AnimationTest/AnimationTest.Build.cs
+++ Source/AnimationTest/AnimationTest.Build.cs
@@ -7,8 +7,8 @@ public class AnimationTest : ModuleRules
 	public AnimationTest(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
--- Source/AnimationTest/BTTask_FindRandomLocation.cpp
+++ Source/AnimationTest/BTTask_FindRandomLocation.cpp
@@ -19,7 +19,7 @@ void UBTTask_FindRandomLocation::OnGameplayTaskActivated(UGameplayTask& Task)
 
 EBTNodeResult::Type UBTTask_FindRandomLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	/*AAIController* AIController = OwnerComp.GetAIOwner();
+	AAIController* AIController = OwnerComp.GetAIOwner();
 	if (AIController)
 	{
 		UBlackboardComponent* Blackboard = OwnerComp.GetBlackboardComponent();
@@ -31,6 +31,6 @@ EBTNodeResult::Type UBTTask_FindRandomLocation::ExecuteTask(UBehaviorTreeCompone
 
 		Blackboard->SetValueAsVector(TEXT("TargetLocation"), PointNavigation.Location);
 		return EBTNodeResult::Succeeded;
-	}*/
+	}
 	return EBTNodeResult::Failed;
 }
